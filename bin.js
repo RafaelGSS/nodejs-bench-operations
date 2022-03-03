81d53f03898094e0019f3331eb6cf81497d9d2b9
RafaelGSS <rafael.nunu@hotmail.com>
2022-03-03 17:23:06 -0300

    update: show machine info

--- bin.js
+++ bin.js
@@ -1,11 +1,19 @@
 const fs = require('fs')
 const path = require('path')
+const os = require('os')
 const { spawnSync } = require('child_process')
 
 const [majorVersion] = process.version.split('.')
+const machineInfo = `${os.platform()} ${os.arch()} | ${os.cpus().length} vCPUs | ${(os.totalmem() / (1024 ** 3)).toFixed(1)}GB Mem`
+
 const writter = fs.createWriteStream(`RESULTS-${majorVersion}.md`)
 
-writter.write('# Node.js Benchmark Operations\n')
+writter.write('# Node.js Benchmark Operations')
+writter.write(`\n
+* __Machine:__ ${machineInfo}
+* __Node:__ \`${process.version}\`
+* __Run:__ ${new Date()}
+`)
 
 fs.readdir(path.join(__dirname, './bench'), (err, files) => {
   for (const file of files) {
