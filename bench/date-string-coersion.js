9f18c4a265e5c06e4c31609e3dcb934e422d9776
RafaelGSS <rafael.nunu@hotmail.com>
2022-03-27 20:23:52 -0300

    feat: add date-string-coersion

---
+++ bench/date-string-coersion.js
@@ -0,0 +1,23 @@
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite;
+
+suite.add('Using String()', function () {
+  const date = new Date()
+  const value = String(date)
+})
+.add('Using `${}`', function () {
+  const date = new Date()
+  const value = `${date}`
+})
+.add('Using \'\' + ', function() {
+  const date = new Date()
+  const value = '' + date
+})
+.add('Using date.toString()', function() {
+  const date = new Date()
+  const value = date.toString()
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.run({ 'async': false });
