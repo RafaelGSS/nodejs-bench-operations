b7a2a6a28303b8dadda0c914d541fa2253499b69
RafaelGSS <rafael.nunu@hotmail.com>
2022-04-07 15:33:27 -0300

    feat: add sort map comparisson

---
+++ bench/sort-map.js
@@ -0,0 +1,26 @@
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite;
+
+const map = new Map()
+map.set('aa', 1)
+map.set('a', 1)
+map.set('c', 1)
+map.set('bb', 1)
+map.set('bd', 1)
+map.set('b', 1)
+
+suite.add('Sort using default', function () {
+  new Map([...map].sort())
+})
+.add('Sort using first char', function () {
+  new Map([...map].sort((a, b) => {
+    return a[0] > b[0] ? -1 : 1
+  }))
+})
+.add('Sort using localeCompare', function () {
+  new Map([...map].sort((a, b) => String(a[0]).localeCompare(b[0])))
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.run({ 'async': false });
