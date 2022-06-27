068fe6924da5e2432a41dbb3da680d3f86e25dd0
RafaelGSS <rafael.nunu@hotmail.com>
2022-06-26 21:27:12 -0300

    update: remove extension

--- bin.js
+++ bin.js
@@ -19,7 +19,7 @@ writter.write(`\n
 fs.readdir(path.join(__dirname, './bench'), (err, files) => {
   for (const file of files) {
     const out = spawnSync(process.execPath, [path.join(__dirname, './bench', file)]).stdout
-    writter.write('\n## ' + file)
+    writter.write('\n## ' + file.split('.')[0])
     writter.write("\n```\n" + out.toString() + "```")
   }
 
