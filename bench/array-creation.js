bf8344d4ec25ffc3b10480e792b05e434b5083cb
RafaelGSS <rafael.nunu@hotmail.com>
2022-07-01 23:33:25 -0300

    feat: add array creation

---
+++ bench/array-creation.js
@@ -0,0 +1,24 @@
+const Benchmark = require('benchmark')
+const suite = new Benchmark.Suite;
+const { eventToMdTable, H2, createTableHeader } = require('../markdown')
+
+const tableHeader = createTableHeader([
+  'name',
+  'ops/sec',
+  'samples'
+])
+
+suite.add('new Array', function () {
+  new Array(1024 * 1024)
+})
+.add('Array.from', function () {
+  Array.from({ length: 1024 * 1024 })
+})
+.on('cycle', function(event) {
+  console.log(eventToMdTable(event))
+})
+.on('start', function() {
+  console.log(H2('Date format MM/DD/YYYY'))
+  console.log(tableHeader)
+})
+.run({ 'async': false });
