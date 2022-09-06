c2ea8961a1a07ad5457a1718b495a27c092e305e
Frazer Smith <frazer.dev@outlook.com>
2022-09-06 18:09:27 +0100

    chore(replaceall): replace value that occurs more than once

--- bench/replace-vs-replaceall-comparison.js
+++ bench/replace-vs-replaceall-comparison.js
@@ -17,16 +17,16 @@ if (semver.lt(versions.node, '15.0.0')) {
   }
 
 suite.add('Using replace(//g)', function () {
-  const text = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'
-  const replaced = text.replace(/dog/g, 'monkey')
+  const text = '1+2+3+4+5+6+7+8+9'
+  const replaced = text.replace(/\+/g, ' ')
 })
 .add('Using replaceAll()', function () {
-  const text = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'
-  const replaced = text.replaceAll('dog', 'monkey')
+  const text = '1+2+3+4+5+6+7+8+9'
+  const replaced = text.replaceAll('+', ' ')
 })
 .add('Using replaceAll(//g)', function () {
-	const text = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'
-	const replaced = text.replaceAll(/dog/g, 'monkey')
+	const text = '1+2+3+4+5+6+7+8+9'
+	const replaced = text.replaceAll(/\+/g, ' ')
   })
 .on('cycle', function(event) {
   console.log(eventToMdTable(event))
