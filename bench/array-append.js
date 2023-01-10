633d58afb1c7403b7be07e2ef0ee3f679e67e557
RafaelGSS <rafael.nunu@hotmail.com>
2023-01-09 23:05:59 -0300

    update: format array-append

--- bench/array-append.js
+++ bench/array-append.js
@@ -1,4 +1,9 @@
-const { H2 } = require('../markdown')
+const { H2, createTableHeader } = require('../markdown')
+
+function compareToMdTable(name, amount, ms) {
+  const numberFormat = new Intl.NumberFormat()
+  return `${name}|${numberFormat.format(amount)}|${numberFormat.format(ms)}ms`
+}
 
 function compare(total) {
   const arrayPush = performance.now();
@@ -15,29 +20,9 @@ function compare(total) {
   }
   const preAllocToal = performance.now() - preAlloc;
 
-  console.log(
-    " " +
-      `
-    ${new Intl.NumberFormat().format(total)} numbers:
-           array.push: ${new Intl.NumberFormat().format(arrayPushTotal)}ms ${
-        arrayPushTotal < preAllocToal ? "(faster)" : ""
-      }
-    new Array(length): ${new Intl.NumberFormat().format(preAllocToal)}ms ${
-        preAllocToal < arrayPushTotal ? "(faster)" : ""
-      }
-  `.trim() +
-      "\n"
-  );
+  console.log(compareToMdTable('array.push', total, arrayPushTotal))
+  console.log(compareToMdTable('new Array(length)', total, preAllocToal))
 }
-
-console.log(H2('Array.append'))
-console.log('```')
-compare(10);
-compare(100);
-compare(1000);
-compare(10000);
-compare(1000000);
-compare(100000000);
 
 function compareStrings(total) {
   const arrayPush = performance.now();
@@ -54,28 +39,30 @@ function compareStrings(total) {
   }
   const preAllocToal = performance.now() - preAlloc;
 
-  console.log(
-    ' ' +
-      `
-    ${new Intl.NumberFormat().format(total)} strings:
-           array.push: ${new Intl.NumberFormat().format(arrayPushTotal)}ms ${
-        arrayPushTotal < preAllocToal ? '(faster)' : ''
-      }
-    new Array(length): ${new Intl.NumberFormat().format(preAllocToal)}ms ${
-        preAllocToal < arrayPushTotal ? '(faster)' : ''
-      }
-  `.trim() +
-      '\n'
-  );
+  console.log(compareToMdTable('array.push', total, arrayPushTotal))
+  console.log(compareToMdTable('new Array(length)', total, preAllocToal))
 }
-console.log("");
-console.log("----");
-console.log("");
-console.log("");
+
+console.log(H2('Array.append (number)'))
+const tableHeader = createTableHeader([
+  'type',
+  'amount',
+  'time elapsed',
+])
+
+console.log(tableHeader)
+compare(10);
+compare(100);
+compare(1000);
+compare(10000);
+compare(1000000);
+compare(100000000);
+
+console.log(H2('Array.append (string)'))
+console.log(tableHeader)
 compareStrings(10);
 compareStrings(100);
 compareStrings(1000);
 compareStrings(10000);
 compareStrings(1000000);
 compareStrings(100000000);
-console.log('```')
