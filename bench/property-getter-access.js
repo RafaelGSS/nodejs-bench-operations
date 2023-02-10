113cd27bce21c6d0a1f7984702cb6e222ca1197c
Vinícius Lourenço <vinyygamerlol@gmail.com>
2023-02-09 22:24:29 -0300

    refactor(add & getter & setter): add more benchmarks

--- bench/property-getter-access.js
+++ bench/property-getter-access.js
@@ -1,12 +1,16 @@
-const Benchmark = require('benchmark')
-const suite = new Benchmark.Suite;
-const { eventToMdTable, H2, createTableHeader } = require('../markdown')
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite();
+const { eventToMdTable, H2, createTableHeader } = require('../markdown');
 
-const tableHeader = createTableHeader([
-  'name',
-  'ops/sec',
-  'samples'
-])
+const tableHeader = createTableHeader(['name', 'ops/sec', 'samples']);
+
+class TestGetter {
+  get test() {
+    return 'Hello';
+  }
+}
+
+const classGetter = new TestGetter();
 
 const getterObj = {
   get test() {
@@ -28,21 +32,137 @@ Object.defineProperty(defineObj, 'test', {
   },
 });
 
+const defineEnumerableFalseObj = {};
+
+Object.defineProperty(defineEnumerableFalseObj, 'test', {
+  get() {
+    return 'Hello';
+  },
+  enumerable: false,
+});
+
+const defineConfigFalseObj = {};
+
+Object.defineProperty(defineConfigFalseObj, 'test', {
+  get() {
+    return 'Hello';
+  },
+  enumerable: true,
+  configurable: false,
+});
+
+const defineEnumerableFalseAndConfigFalseObj = {};
+
+Object.defineProperty(defineEnumerableFalseAndConfigFalseObj, 'test', {
+  get() {
+    return 'Hello';
+  },
+  enumerable: false,
+  configurable: false,
+});
+
+const defineOnlyWritableObj = {};
+
+Object.defineProperty(defineOnlyWritableObj, 'test', {
+  writable: true,
+  value: 'Hello',
+});
+
+const defineWritableEnumerableFalseObj = {};
+
+Object.defineProperty(defineWritableEnumerableFalseObj, 'test', {
+  writable: true,
+  value: 'Hello',
+  enumerable: false,
+});
+
+const defineWritableEnumerableFalseAndConfigFalseObj = {};
+
+Object.defineProperty(defineWritableEnumerableFalseAndConfigFalseObj, 'test', {
+  writable: true,
+  value: 'Hello',
+  enumerable: false,
+  configurable: false,
+});
+
+const definePropertiesObj = {};
+
+Object.defineProperties(definePropertiesObj, {
+  test: {
+    get() {
+      return 'Hello';
+    },
+  },
+});
+
+const definePropertiesEnumerableFalseObj = {};
+
+Object.defineProperties(definePropertiesEnumerableFalseObj, {
+  test: {
+    get() {
+      return 'Hello';
+    },
+    enumerable: false,
+  },
+});
+
+const definePropertiesEnumerableFalseAndConfigFalseObj = {};
+
+Object.defineProperties(definePropertiesEnumerableFalseAndConfigFalseObj, {
+  test: {
+    get() {
+      return 'Hello';
+    },
+    enumerable: false,
+    configurable: false,
+  },
+});
+
 suite
-.add('Getter', function () {
-  const v = getterObj.test;
-})
-.add('Method', function () {
-  const v = methodObj.test();
-})
-.add('DefineProperty', function () {
-  const v = defineObj.test;
-})
-.on('cycle', function(event) {
-  console.log(eventToMdTable(event))
-})
-.on('start', function() {
-  console.log(H2('Property Getter Access'))
-  console.log(tableHeader)
-})
-.run({ 'async': false });
+  .add('Getter (class)', function () {
+    const v = classGetter.test;
+  })
+  .add('Getter', function () {
+    const v = getterObj.test;
+  })
+  .add('Method', function () {
+    const v = methodObj.test();
+  })
+  .add('DefineProperty (getter)', function () {
+    const v = defineObj.test;
+  })
+  .add('DefineProperty (getter & enumerable=false)', function () {
+    const v = defineEnumerableFalseObj.test;
+  })
+  .add('DefineProperty (getter & configurable=false)', function () {
+    const v = defineConfigFalseObj.test;
+  })
+  .add('DefineProperty (getter & enumerable=false & configurable=false)', function () {
+    const v = defineEnumerableFalseAndConfigFalseObj.test;
+  })
+  .add('DefineProperty (writable)', function () {
+    const v = defineOnlyWritableObj.test;
+  })
+  .add('DefineProperty (writable & enumerable=false)', function () {
+    const v = defineWritableEnumerableFalseObj.test;
+  })
+  .add('DefineProperty (writable & enumerable=false & configurable=false)', function () {
+    const v = defineWritableEnumerableFalseAndConfigFalseObj.test;
+  })
+  .add('DefineProperties (getter)', function () {
+    const v = definePropertiesObj.test;
+  })
+  .add('DefineProperties (getter & enumerable=false)', function () {
+    const v = definePropertiesEnumerableFalseObj.test;
+  })
+  .add('DefineProperties (getter & enumerable=false & configurable=false)', function () {
+    const v = definePropertiesEnumerableFalseAndConfigFalseObj.test;
+  })
+  .on('cycle', function (event) {
+    console.log(eventToMdTable(event));
+  })
+  .on('start', function () {
+    console.log(H2('Property Getter Access'));
+    console.log(tableHeader);
+  })
+  .run({ async: false });
