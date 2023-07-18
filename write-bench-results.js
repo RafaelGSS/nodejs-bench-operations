687f0e4b256608750bb3c22a29ec6cb4ee579003
Vinícius Lourenço <vinyygamerlol@gmail.com>
2023-07-17 21:26:43 -0300

    fixup! ci: added reusable workflow to run benchmarks

--- write-bench-results.js
+++ write-bench-results.js
@@ -3,9 +3,19 @@ const path = require('node:path');
 
 const benchResult = JSON.parse(process.env.BENCH_RESULT);
 
+function getBenchmarkFile(key) {
+  const filepath = process.env.BENCH_FILE || key.split(':')[0];
+
+  return path.basename(filepath, '.js');
+}
+
+function getBenchmarkNodeVersion(key) {
+  return process.env.BENCH_FILE ? key : key.split(':')[1];
+}
+
 for (const key of Object.keys(benchResult.result)) {
-  const [benchmarkFilepath, nodeVersion] = key.split(':');
-  const benchFile = path.basename(benchmarkFilepath, '.js');
+  const benchFile = getBenchmarkFile(key);
+  const nodeVersion = getBenchmarkNodeVersion(key);
 
   const major = nodeVersion.split('.')[0];
   const result = decodeURIComponent(benchResult.result[key]).replace(/\\#/g, '#');
