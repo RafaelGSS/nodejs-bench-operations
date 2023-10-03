e6917c86fb666fe439198773866d517201a9c14f
RafaelGSS <rafael.nunu@hotmail.com>
2023-10-02 21:46:54 -0300

    update: cleanup http

--- http/core.js
+++
@@ -1,9 +0,0 @@
-const http = require('node:http');
-
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
-
-const server = http.createServer(requestListener);
-server.listen(3000);
