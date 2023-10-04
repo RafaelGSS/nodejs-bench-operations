eaad719721fea4df51e4d77840159adc33f384fe
Vinícius Lourenço <contact@viniciusl.com.br>
2023-10-04 20:53:52 -0300

    ci(bench & watch_bench): fix output size limit

--- scripts/write-bench-results.mjs
+++ scripts/write-bench-results.mjs
@@ -1,8 +1,25 @@
-import { mkdir, writeFile } from 'node:fs/promises';
+import { mkdir, writeFile, rename } from 'node:fs/promises';
 import { basename, resolve } from 'node:path';
 import { existAsync, rootFolder } from './utils.mjs';
+import { create as createArtifactClient } from '@actions/artifact';
+import { readFileSync } from 'node:fs';
 
-const benchResult = JSON.parse(process.env.BENCH_RESULT);
+/**
+ * The object result with the artifacts names
+ * The result will be a record with the possible states:
+ * 
+ * ```js
+ * // when is called by bench.yml
+ * { '18.18.0': 'benchmark-1-18.18.0-add-property.js.md' }
+ * // when is called by watch_bench.yml
+ * { 'add-property.js:18.18.0': 'benchmark-1-18.18.0-add-property.js.md' }
+ * ```
+ * 
+ * @type {{ result: { [nodeVersion: string]: string } }}
+ */
+const benchResult = JSON.parse(process.env.BENCH_ARTIFACTS);
+
+const artifactClient = createArtifactClient();
 
 function getBenchmarkFile(key) {
   const filepath = process.env.BENCH_FILE || key.split(':')[0];
@@ -19,7 +36,9 @@ for (const key of Object.keys(benchResult.result)) {
   const nodeVersion = getBenchmarkNodeVersion(key).replace(/\./g, '_');
 
   const major = nodeVersion.split('_')[0];
-  const result = Buffer.from(benchResult.result[key], 'base64').toString('utf8');
+  const artifactKey = benchResult.result[key];
+
+  const { downloadPath } = await artifactClient.downloadArtifact(artifactKey, `./${ artifactKey }`);
   const outputFolder = resolve(rootFolder, `./v${major}/v${nodeVersion}`);
 
   const outputFolderExist = await existAsync(outputFolder);
@@ -30,5 +49,5 @@ for (const key of Object.keys(benchResult.result)) {
     });
   }
 
-  await writeFile(`${outputFolder}/${benchFile}.md`, result, 'utf-8');
+  await rename(downloadPath, `${outputFolder}/${benchFile}.md`, 'utf-8');
 }
