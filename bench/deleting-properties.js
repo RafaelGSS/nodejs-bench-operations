4e8ca696d0b0d74cdbc50a8f363797344cfdc777
Vinícius Lourenço <contact@viniciusl.com.br>
2023-10-10 00:01:31 -0300

    refactor(deleting-properties): added more benchmarks

--- bench/deleting-properties.js
+++ bench/deleting-properties.js
@@ -2,6 +2,17 @@ const { createBenchmarkSuite } = require('../common')
 
 const suite = createBenchmarkSuite('Deleting properties')
 
+const NullObject = function NullObject () { }
+NullObject.prototype = Object.create(null)
+
+const setupForMap = function () {
+  const data = new Map();
+  
+  for (let i = 0; i < this.count; i++) {
+    data.set(i.toString(), i);
+  }
+}
+
 suite
   .add('Using delete property', function () {
     const data = { x: 1, y: 2, z: 3 }
@@ -11,8 +22,55 @@ suite
     data.y
     data.z
   })
+  .add('Using delete property (proto: null)', function () {
+    const data = { __proto__: null, x: 1, y: 2, z: 3 }
+    delete data.y
+
+    data.x
+    data.y
+    data.z
+  })
+  .add('Using delete property (cached proto: null)', function () {
+    const data = new NullObject()
+
+    data.x = 1
+    data.y = 2
+    data.z = 3
+
+    delete data.y
+
+    data.x
+    data.y
+    data.z
+  })
+  .add('Using map.delete', function () {
+    data.delete(this.count.toString())
+  }, {
+    setup: setupForMap,
+  })
   .add('Using undefined assignment', function () {
     const data = { x: 1, y: 2, z: 3 }
+    data.y = undefined
+
+    data.x
+    data.y
+    data.z
+  })
+  .add('Using undefined assignment (proto: null)', function () {
+    const data = { __proto__: null, x: 1, y: 2, z: 3 }
+    data.y = undefined
+
+    data.x
+    data.y
+    data.z
+  })
+  .add('Using undefined property (cached proto: null)', function () {
+    const data = new NullObject()
+
+    data.x = 1
+    data.y = 2
+    data.z = 3
+
     data.y = undefined
 
     data.x
