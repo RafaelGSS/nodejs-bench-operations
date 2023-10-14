64c43346bdc3ad4bf779b01dec5d5591d9a308c5
Vinícius Lourenço <contact@viniciusl.com.br>
2023-10-13 22:54:56 -0300

    feat(run-all): added support to customize node version

--- scripts/generate-run-all.mjs
+++ scripts/generate-run-all.mjs
@@ -21,6 +21,7 @@ const benchJobs = allBenches.map((benchFile, index, array) => {
     uses: ./.github/workflows/bench.yml
     with:
       bench-file: ${benchFile}
+      node-versions: \${{ inputs.node-versions }}
 `;
 });
 
@@ -29,6 +30,12 @@ name: 'Run All'
 
 on:
   workflow_dispatch:
+    inputs:
+      node-versions:
+        required: true
+        type: string
+        default: '[16.0.0, 16.18.1, 16.19.0, 16.20.2, 18.0.0, 18.16.1, 18.17.1, 18.18.0, 19.0.0, 19.9.0, 20.0.0, 20.6.0, 20.7.0, 20.8.0]'
+        description: 'The Node.js Versions (should be a JSON array)'
 
 permissions:
   contents: write
