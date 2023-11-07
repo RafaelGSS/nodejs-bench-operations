c4c6decaeff36ebde7387360e5950cd1aadda571
RafaelGSS <rafael.nunu@hotmail.com>
2023-11-07 13:54:49 +0000

    workflow: add continue-on-error

--- scripts/generate-run-all.mjs
+++ scripts/generate-run-all.mjs
@@ -19,6 +19,7 @@ const benchJobs = allBenches.map((benchFile, index, array) => {
     ${ index > 0 ? `needs: ${formatJobName(array[index - 1])}` : '' }
     name: Running "${benchFile}"
     uses: ./.github/workflows/bench.yml
+    continue-on-error: true
     with:
       bench-file: ${benchFile}
       node-versions: \${{ inputs.node-versions }}
