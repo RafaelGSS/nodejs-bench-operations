c984ff9f28cfb82b3f0f8df81782d21d7c8ff6b8
Gürgün Dayıoğlu <hey@gurgun.day>
2024-04-23 15:24:19 +0200

    add string stuff

---
+++ bench/string-startsWith.js
@@ -0,0 +1,44 @@
+const { createBenchmarkSuite } = require('../common')
+
+const suite = createBenchmarkSuite('Check if string starts with a substring')
+const shortString = 'foobar'
+const longString = 'foobar'.repeat(100)
+
+const comparison = 'foo'
+const comparison2 = 'bar'
+
+const startsWithRegExp = new RegExp(`^${comparison}`)
+
+suite.add('(short string) (true) String#startsWith', function () {
+    shortString.startsWith(comparison)
+    }
+).add('(short string) (true) String#slice and strict comparison', function () {
+    shortString.slice(0, comparison.length) === comparison
+    }
+).add('(short string) (true) RegExp#test', function () {
+    startsWithRegExp.test(shortString)
+    }
+)
+.add('(long string) (true) String#startsWith', function () {
+    longString.startsWith(comparison)
+    }
+).add('(long string) (true) String#slice and strict comparison', function () {
+    longString.slice(0, comparison.length) === comparison
+    }
+).add('(long string) (true) RegExp#test', function () {
+    startsWithRegExp.test(longString)
+    }
+)
+.add('(short string) (false) String#startsWith', function () {
+    shortString.startsWith(comparison2)
+    }
+).add('(short string) (false) String#slice and strict comparison', function () {
+    shortString.slice(0, comparison2.length) === comparison2
+    }
+).add('(long string) (false) String#startsWith', function () {
+    longString.startsWith(comparison2)
+    }
+).add('(long string) (false) String#slice and strict comparison', function () {
+    longString.slice(0, comparison2.length) === comparison2
+    }
+).run({ async: false })
