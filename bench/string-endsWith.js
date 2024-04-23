d581d4d049ce726321f78fff693f4b6e16c2c0be
Gürgün Dayıoğlu <hey@gurgun.day>
2024-04-23 15:57:30 +0200

    format

--- bench/string-endsWith.js
+++ bench/string-endsWith.js
@@ -8,28 +8,37 @@ const comparison = 'bar'
 const comparison2 = 'foo'
 
 
-suite.add('(short string) (true) String#endsWith', function () {
+suite
+.add('(short string) (true) String#endsWith', function () {
     shortString.endsWith(comparison)
     }
-).add('(short string) (true) String#slice and strict comparison', function () {
+)
+.add('(short string) (true) String#slice and strict comparison', function () {
     shortString.slice(-comparison.length) === comparison
     }
-).add('(long string) (true) String#endsWith', function () {
+)
+.add('(long string) (true) String#endsWith', function () {
     longString.endsWith(comparison)
     }
-).add('(long string) (true) String#slice and strict comparison', function () {
+)
+.add('(long string) (true) String#slice and strict comparison', function () {
     longString.slice(-comparison.length) === comparison
     }
-).add('(short string) (false) String#endsWith', function () {
+)
+.add('(short string) (false) String#endsWith', function () {
     shortString.endsWith(comparison2)
     }
-).add('(short string) (false) String#slice and strict comparison', function () {
+)
+.add('(short string) (false) String#slice and strict comparison', function () {
     shortString.slice(-comparison2.length) === comparison2
     }
-).add('(long string) (false) String#endsWith', function () {
+)
+.add('(long string) (false) String#endsWith', function () {
     longString.endsWith(comparison2)
     }
-).add('(long string) (false) String#slice and strict comparison', function () {
+)
+.add('(long string) (false) String#slice and strict comparison', function () {
     longString.slice(-comparison2.length) === comparison2
     }
-).run({ async: false })
+)
+.run({ async: false })
